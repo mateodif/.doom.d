c7cf3aa4a9db1d89b56e743b6f9f6d6b21629561
Mateo Difranco <mateodifr@gmail.com>
2022-08-18 19:27:57 -0300

    Change font to Terminus

--- config.el
+++ config.el
@@ -20,9 +20,9 @@
 ;;
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
-;;
-(setq doom-font (font-spec :family "Lilex" :size 16))
-;;
+
+(setq doom-font (font-spec :family "Terminus" :size 12))
+
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely

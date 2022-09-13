251df6ff6c7f3d7510095842ef9e2ddd06ac0d3b
Mateo Difranco <mateodifr@gmail.com>
2022-09-12 23:23:43 -0300

    New defaults: IBM Plex (font), minimal-light (theme)

--- config.el
+++ config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 
-(setq doom-font (font-spec :family "Terminus" :size 12))
+(setq doom-font (font-spec :family "IBM Plex Mono" :size 17))
 
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -31,7 +31,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one-light)
+(setq doom-theme 'minimal-light)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
